7c7a69dda50e0dc3094ad58cd20f496e8067e6a6
ngtdungdev <dungboi1029@gmail.com>
2024-03-31 03:48:35 +0700

    signin and signUp

---
+++ src/pages/Login/Components/SignIn/index.jsx
@@ -0,0 +1,102 @@
+import {useEffect, useRef, useState} from "react";
+import {Link, useNavigate} from "react-router-dom";
+import styles  from "../../Login.module.scss"
+import classNames from "classnames/bind"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRightLong } from '@fortawesome/free-solid-svg-icons';
+import { FcGoogle } from "react-icons/fc";
+import {login, signInWithGoogle, signUp} from "../../../../utils/firebase/auth";
+
+const SignIn = ({OnClickSignUp}) => {
+    const cx = classNames.bind(styles)
+    const [isLoggingIn, setIsLoggingIn] = useState(false);
+    const [errorMessage, setErrorMessage] = useState("");
+    const onGoogleSignIn = async (e) => {
+        e.preventDefault();
+        if (isLoggingIn)
+            return;
+        setIsLoggingIn(true);
+        try {
+            const user = await signInWithGoogle();
+            await login(user,
+                () => setErrorMessage("Hệ thống đang bảo trì, vui lòng thử lại sau."),
+                () => signUp(user)
+            );
+        } catch (error) {
+            setErrorMessage("Vui lòng kiểm tra kết nối mạng.");
+        }
+        setIsLoggingIn(false);
+    };
+
+
+    return (
+        <div className={cx("login-cart-content")}>
+            <div className={cx("main-cart-section")}>
+                <div className={cx("headings-container")}>
+                    <div>
+                        <h1 className={cx("ui-heading")}>Log in</h1>
+                        <h3 className={cx("ui-subheading")}>Continue to Laptop account</h3>
+                    </div>
+                </div>
+                <div className={cx("captcha-element")}></div>
+                <form className={cx("web_authn_form")}></form>
+                <form className={cx("account-lookup")}>
+                    <div className={cx("combined-email")}>
+                        <div className={cx("next-email")}>
+                            <label className={cx("next-email-label")}>Email</label>
+                            <div className={cx("next-input")}>
+                                <div className={cx("combined-input")}>
+                                    <input className={cx("email")}/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={cx("combined-password")}>
+                        <div className={cx("next-password")}>
+                            <label className={cx("next-password-label")}>Password</label>
+                            <div className={cx("next-input")}>
+                                <div className={cx("combined-input")}>
+                                    <input className={cx("password")}/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={cx("button-login-container")}>
+                        <button className={cx("ui-button")}>
+                            <span className={cx("content")}>
+                                <span className={cx("ui-button-text")}>Login</span>
+                                <span className={cx("ui-button-hover-icon")}>
+                                    <FontAwesomeIcon icon={faArrowRightLong}
+                                                     className={cx("icons")}/>
+                                </span>
+                            </span>
+                        </button>
+                    </div>
+                </form>
+                <div className={cx("external-login-divider-container")}>
+                    <div className={cx("external-login-divider")}>
+                        <div className={cx("external-login-divider-text")}>or
+                        </div>
+                    </div>
+                </div>
+                <div className={cx("external-login-providers")}>
+                    <button className={cx('ui-button-google')} onClick={e => onGoogleSignIn(e)}>
+                        <span className={cx('button-google-text')}>Đăng nhập bằng Google</span>
+                        <span className={cx('button-google-icon')}><FcGoogle/></span>
+                    </button>
+                </div>
+                <p className={cx("help-link")}>
+                    <span className={cx("help-link-text")}>New to Laptop?</span>
+                    <Link className={cx("ui-arrow-link")} to={"/signup"} onClick={OnClickSignUp}>
+                        Get started
+                        <span className={cx("arrow-link-icon")}>
+                            <FontAwesomeIcon icon={faArrowRightLong} className={cx("icons")}/>
+                        </span>
+                    </Link>
+                </p>
+            </div>
+        </div>
+    )
+}
+
+export default SignIn;
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -8,11 +8,17 @@ import { faArrowRightLong } from '@fortawesome/free-solid-svg-icons';
 import { FcGoogle } from "react-icons/fc";
 import {useAuth} from "../../contexts/AuthContext";
 import {login, signInWithGoogle, signUp} from "../../utils/firebase/auth";
+import SignUp from "../SignUp";
+import SignIn from "./Components/SignIn";
 
-const Login = () => {
+const Login = ({active}) => {
     const navigate = useNavigate();
     const {userLoggedIn} = useAuth();
 
+    const [isSignIn, setIsSignIn] = useState(active);
+    const handleIsSign = () => {
+        setIsSignIn((prevSign) => !prevSign);
+    }
 
     const cx = classNames.bind(styles)
     const [email, setEmail] = useState("");
@@ -70,92 +76,127 @@ const Login = () => {
             <div className={cx("main-page")}>
                 <div className={cx("page-content")}>
                 <div className={cx("main-content")}>
-                        <div className={cx("login-cart")}>
-                            <div className={cx("login-cart-helder")}>
-                                <h1 className={cx("login-cart-logo")}>
-                                    <header className={cx("loginCart-helder")}>
-                                        <Link to={"\"/signup"}>
-                                            <img src={logo} alt={""}/>
-                                        </Link>
-                                    </header>
-                                    LapTop
-                                </h1>
-                            </div>
-                            <div className={cx("login-cart-content")}>
-                                <div className={cx("main-cart-section")}>
-                                    <div className={cx("headings-container")}>
-                                        <div>
-                                            <h1 className={cx("ui-heading")}>Log in</h1>
-                                            <h3 className={cx("ui-subheading")}>Continue to Laptop account</h3>
-                                        </div>
-                                    </div>
-                                    <div className={cx("captcha-element")}></div>
-                                    <form className={cx("web_authn_form")}></form>
-                                    <form className={cx("account-lookup")}>
-                                        <div className={cx("combined-email")}>
-                                            <div className={cx("next-email")}>
-                                                <label className={cx("next-email-label")}>Email</label>
-                                                <div className={cx("next-input")}>
-                                                    <div className={cx("combined-input")}>
-                                                        <input className={cx("email")}/>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div className={cx("combined-password")}>
-                                            <div className={cx("next-password")}>
-                                                <label className={cx("next-password-label")}>Password</label>
-                                                <div className={cx("next-input")}>
-                                                    <div className={cx("combined-input")}>
-                                                        <input className={cx("password")}/>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                        <div className={cx("button-login-container")}>
-                                            <button className={cx("ui-button")}>
-                                                <span className={cx("content")}>
-                                                    <span className={cx("ui-button-text")}>Login</span>
-                                                    <span className={cx("ui-button-hover-icon")}>
-                                                        <FontAwesomeIcon icon={faArrowRightLong} className={cx("icons")}/>
-                                                    </span>
-                                                </span>
-                                            </button>
-                                        </div>
-                                    </form>
-                                    <div className={cx("external-login-divider-container")}>
-                                        <div className={cx("external-login-divider")}>
-                                            <div className={cx("external-login-divider-text")}>or
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className={cx("external-login-providers")}>
-                                        {/*<div className={cx('button-google')}>*/}
-                                            <button className={cx('ui-button-google')}>
-                                                <span className={cx('button-google-text')}>Đăng nhập bằng Google</span>
-                                                <span className={cx('button-google-icon')}><FcGoogle/></span>
-                                            </button>
-                                        {/*</div>*/}
-                                    </div>
-                                    <p className={cx("help-link")}>
-                                        <span className={cx("help-link-text")}>New to Laptop?</span>
-                                        <Link className={cx("ui-arrow-link")} to={"\"/signup"}>
-                                            Get started
-                                            <span className={cx("arrow-link-icon")}>
-                                                <FontAwesomeIcon icon={faArrowRightLong} className={cx("icons")}/>
-                                            </span>
-                                        </Link>
-                                    </p>
-                                </div>
-                            </div>
+                    <div className={cx("login-cart")}>
+                        <div className={cx("login-cart-helder")}>
+                            <h1 className={cx("login-cart-logo")}>
+                                <header className={cx("loginCart-helder")}>
+                                    <Link to={"/login"}>
+                                        <img src={logo} alt={""}/>
+                                    </Link>
+                                </header>
+                                LapTop
+                            </h1>
                         </div>
-                        <footer className={cx("login-footer")}>
-                            <div className={cx("login-footer-link")}>
-                                <Link to={"\"/login"} className={cx("login-link")}>Help</Link>
-                                <Link to={"\"/login"} className={cx("login-link")}>Privacy</Link>
-                                <Link to={"\"/login"} className={cx("login-link")}>Terms</Link>
-                            </div>
-                        </footer>
+                        {
+                            isSignIn ? <SignIn OnClickSignUp={handleIsSign} /> : <SignUp OnClickSignIn={handleIsSign} />
+                        }
+                        <div>
+                            {/*<div className={cx("login-cart-content")}>*/}
+                            {/*    <div className={cx("main-cart-section")}>*/}
+                            {/*        <div className={cx("headings-container")}>*/}
+                            {/*            <div>*/}
+                            {/*                <h1 className={cx("ui-heading")}>Log in</h1>*/}
+                            {/*                <h3 className={cx("ui-subheading")}>Continue to Laptop account</h3>*/}
+                            {/*            </div>*/}
+                            {/*        </div>*/}
+                            {/*        <div className={cx("captcha-element")}></div>*/}
+                            {/*        <form className={cx("web_authn_form")}></form>*/}
+                            {/*        <form className={cx("account-lookup")}>*/}
+                            {/*            <div className={cx("combined-email")}>*/}
+                            {/*                <div className={cx("next-email")}>*/}
+                            {/*                    <label className={cx("next-email-label")}>Email</label>*/}
+                            {/*                    <div className={cx("next-input")}>*/}
+                            {/*                        <div className={cx("combined-input")}>*/}
+                            {/*                            <input className={cx("email")}/>*/}
+                            {/*                        </div>*/}
+                            {/*                    </div>*/}
+                            {/*                </div>*/}
+                            {/*            </div>*/}
+                            {/*            <div className={cx("combined-password")}>*/}
+                            {/*                <div className={cx("next-password")}>*/}
+                            {/*                    <label className={cx("next-password-label")}>Password</label>*/}
+                            {/*                    <div className={cx("next-input")}>*/}
+                            {/*                        <div className={cx("combined-input")}>*/}
+                            {/*                            <input className={cx("password")}/>*/}
+                            {/*                        </div>*/}
+                            {/*                    </div>*/}
+                            {/*                </div>*/}
+                            {/*            </div>*/}
+                            {/*            <div className={cx("button-login-container")}>*/}
+                            {/*                {*/}
+                            {/*                    isSignIn ? <>*/}
+                            {/*                            <button className={cx("ui-button")} onClick={handleIsSign}>*/}
+                            {/*                                <span className={cx("content")}>*/}
+                            {/*                                    <span className={cx("ui-button-text")}>Login</span>*/}
+                            {/*                                    <span className={cx("ui-button-hover-icon")}>*/}
+                            {/*                                        <FontAwesomeIcon icon={faArrowRightLong} className={cx("icons")}/>*/}
+                            {/*                                    </span>*/}
+                            {/*                                </span>*/}
+                            {/*                            </button>*/}
+                            {/*                        </> :*/}
+                            {/*                        <>*/}
+                            {/*                            <button className={cx("ui-button")} onClick={handleIsSign}>*/}
+                            {/*                                <span className={cx("content")}>*/}
+                            {/*                                    <span className={cx("ui-button-text")}>Create Laptop account</span>*/}
+                            {/*                                    <span className={cx("ui-button-hover-icon")}>*/}
+                            {/*                                        <FontAwesomeIcon icon={faArrowRightLong} className={cx("icons")}/>*/}
+                            {/*                                    </span>*/}
+                            {/*                                </span>*/}
+                            {/*                            </button>*/}
+                            {/*                        </>*/}
+                            {/*                }*/}
+                            {/*            </div>*/}
+                            {/*        </form>*/}
+                            {/*        <div className={cx("external-login-divider-container")}>*/}
+                            {/*            <div className={cx("external-login-divider")}>*/}
+                            {/*                <div className={cx("external-login-divider-text")}>or*/}
+                            {/*                </div>*/}
+                            {/*            </div>*/}
+                            {/*        </div>*/}
+                            {/*        <div className={cx("external-login-providers")}>*/}
+                            {/*            /!*<div className={cx('button-google')}>*!/*/}
+
+                            {/*                <button className={cx('ui-button-google')} onClick={e => onGoogleSignIn(e)}>*/}
+                            {/*                    <span className={cx('button-google-text')}>Đăng nhập bằng Google</span>*/}
+                            {/*                    <span className={cx('button-google-icon')}><FcGoogle/></span>*/}
+                            {/*                </button>*/}
+                            {/*            /!*</div>*!/*/}
+                            {/*        </div>*/}
+                            {/*        {*/}
+                            {/*            isSignIn ? <>*/}
+                            {/*                    <p className={cx("help-link")}>*/}
+                            {/*                        <span className={cx("help-link-text")}>New to Laptop?</span>*/}
+                            {/*                        <Link className={cx("ui-arrow-link")} to={"\"/signup"}>*/}
+                            {/*                            Get started*/}
+                            {/*                            <span className={cx("arrow-link-icon")}>*/}
+                            {/*                                <FontAwesomeIcon icon={faArrowRightLong} className={cx("icons")}/>*/}
+                            {/*                            </span>*/}
+                            {/*                        </Link>*/}
+                            {/*                    </p>*/}
+                            {/*                </> :*/}
+                            {/*                <>*/}
+                            {/*                    <p className={cx("help-link")}>*/}
+                            {/*                        <span className={cx("help-link-text")}>Already have a Shopify account?</span>*/}
+                            {/*                        <Link className={cx("ui-arrow-link")} to={"\"/signup"}>*/}
+                            {/*                            Login*/}
+                            {/*                            <span className={cx("arrow-link-icon")}>*/}
+                            {/*                                <FontAwesomeIcon icon={faArrowRightLong} className={cx("icons")}/>*/}
+                            {/*                            </span>*/}
+                            {/*                        </Link>*/}
+                            {/*                    </p>*/}
+                            {/*                </>*/}
+                            {/*        }*/}
+                            {/*    </div>*/}
+                            {/*</div>*/}
+                        </div>{/*note*/}
+                    </div>
+                    <footer className={cx("login-footer")}>
+                        <div className={cx("login-footer-link")}>
+                            <Link to={"/login"} className={cx("login-link")}>Help</Link>
+                            <Link to={"/login"} className={cx("login-link")}>Privacy</Link>
+                            <Link to={"/login"} className={cx("login-link")}>Terms</Link>
+                        </div>
+                    </footer>
                     </div>
                 </div>
             </div>
