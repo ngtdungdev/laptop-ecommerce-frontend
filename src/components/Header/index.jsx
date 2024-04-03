013713ab0c0742ec992489a539396ffdc52a0c13
ngtdungdev <dungboi1029@gmail.com>
2024-04-04 03:17:37 +0700

    shopping

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -8,25 +8,27 @@ const Header = () => {
     return (
         <div>
             <div className={cx("page-container")}>
-                <div className={cx("gradient-background-wrapper")}>
-                    <div className={cx("gradient-background")}>
-                        <div className={cx("gradient-background-left")}></div>
-                        <div className={cx("gradient-background-right")}></div>
+                <div className={cx("ui-page-container")}>
+                    <div className={cx("gradient-background-wrapper")}>
+                        <div className={cx("gradient-background")}>
+                            <div className={cx("gradient-background-left")}></div>
+                            <div className={cx("gradient-background-right")}></div>
+                        </div>
+                        <div className={cx("gradient-background-noise")}></div>
                     </div>
-                    <div className={cx("gradient-background-noise")}></div>
-                </div>
-                <div className={cx("main-page")}>
-                    <div className={cx("cart-helder")}>
-                        <h1 className={cx("cart-logo")}>
-                            <header className={cx("loginCart-helder")}>
-                                <Link to={""}>
-                                    <img src={logo} alt={""}/>
-                                </Link>
-                            </header>
-                        </h1>
-                    </div>
-                    <div className={cx("navbar")}>
-                        <Navbar/>
+                    <div className={cx("main-page")}>
+                        <div className={cx("cart-helder")}>
+                            <h1 className={cx("cart-logo")}>
+                                <header className={cx("loginCart-helder")}>
+                                    <Link to={""}>
+                                        <img src={logo} alt={""}/>
+                                    </Link>
+                                </header>
+                            </h1>
+                        </div>
+                        <div className={cx("navbar")}>
+                            <Navbar/>
+                        </div>
                     </div>
                 </div>
             </div>
--- src/pages/Shop/index.jsx
+++ src/pages/Shop/index.jsx
@@ -1,10 +1,51 @@
-import styles  from "./shop.module.scss"
+import styles  from "./Shop.module.scss"
+import component  from "../../layouts/component.module.scss"
 import classNames from "classnames/bind"
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import search from "../../assets/images/search.svg";
+import {faAngleDown} from "@fortawesome/free-solid-svg-icons";
+
 const Shop = () => {
     const cx = classNames.bind(styles)
+    const cd = classNames.bind(component)
     return (
         <div>
-            Shop
+            <div className={cx("shop-container")}>
+                <div className={cx("ui-shop")}>
+                    <div className={cx("ui-shop-top")}>
+                        <div className={cx("ui-search", "ui-option")}>
+                            <div className={`${cx("next-input")} ${cd("next-input")}`}>
+                                <div className={`${cx("combined-input")} ${cd("combined-input")}`}>
+                                    <input className={`${cx("input")} ${cd("input")}`} placeholder={"Search"}/>
+                                    <div className={cx("ui-search")}>
+                                        <img src={search} alt={""}/>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={cx("ui-combobox", "ui-option")}>
+                            <div className={cx("ui-select-container")}>
+                                <div className={cx("ui-select")}>
+                                    <p className={cx("select")}>LapTop</p>
+                                    <FontAwesomeIcon icon={faAngleDown} className={cx("img")}/>
+                                </div>
+                                <div className={cx("list-option")}>
+                                    <div className={"ui-option"}>LapTop</div>
+                                    <div className={"ui-option"}>Mouse</div>
+                                    <div className={"ui-option"}>KeyBoard</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={cx("ui-tow-bar", "ui-option")}>
+
+                        </div>
+                    </div>
+                    <div className={cx("ui-shop-bottom")}>
+
+                    </div>
+                </div>
+            </div>
+
         </div>
     );
 };
